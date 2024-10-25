5dbd65cef3d11b4f9726258434073c081b1d1951
Ben12002 <bendamian2012@gmail.com>
2024-10-25 19:20:36 +0700

    Added styling

--- learn-forms/app.js
+++ learn-forms/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
+const path = require("node:path");
 const app = express();
 const usersRouter = require("./routes/usersRouter");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
