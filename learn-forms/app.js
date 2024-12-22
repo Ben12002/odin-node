241fa7633ac50ff7ef14e2530763cdcc03500cd8
Ben12002 <bendamian2012@gmail.com>
2024-12-22 11:34:22 -0800

    port

--- learn-forms/app.js
+++ learn-forms/app.js
@@ -10,5 +10,5 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use("/", usersRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
